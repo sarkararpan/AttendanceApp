962625b946f141dfbb36ec3b70a97a33881d3baf
Aurghyadip Kundu <adkundu@gmail.com>
2018-03-29 11:25:23 +0530

    date picker and proper date parsing added

--- app/src/main/java/att2/com/wrc/aas/TeacherIntermediateActivity.java
+++ app/src/main/java/att2/com/wrc/aas/TeacherIntermediateActivity.java
@@ -1,18 +1,28 @@
 package att2.com.wrc.aas;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
+import java.util.Calendar;
+
 public class TeacherIntermediateActivity extends AppCompatActivity {
+
+    private static final String TAG = "TeacherIntermediate";
 
     EditText periodField;
     EditText dateField;
     EditText classField;
     Button submitBtn;
+
+    private DatePickerDialog.OnDateSetListener onDateSetListener;
+    String date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +38,32 @@ public class TeacherIntermediateActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+
+        dateField.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        TeacherIntermediateActivity.this,
+                        android.R.style.Theme_Material_Dialog_MinWidth,
+                        onDateSetListener,
+                        year, month, day);
+                dialog.show();
+            }
+        });
+
+        onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+                date = dayOfMonth + "-" + month + "-" + year;
+                dateField.setText(date);
+            }
+        };
 
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
