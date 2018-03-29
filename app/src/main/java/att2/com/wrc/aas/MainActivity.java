ab8af8546b6de3cebeb06efc764f33400fbaa13c
Aurghyadip Kundu <adkundu@gmail.com>
2018-03-29 23:38:11 +0530

    Added functionality to link

--- app/src/main/java/att2/com/wrc/aas/MainActivity.java
+++ app/src/main/java/att2/com/wrc/aas/MainActivity.java
@@ -5,20 +5,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 // TODO: Add proper navigation
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView teacherViewLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        teacherViewLink = findViewById(R.id.teacher_view_link);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+
+        teacherViewLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TeacherIntermediateActivity.class));
+            }
+        });
     }
 }
