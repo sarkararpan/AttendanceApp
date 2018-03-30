6963c2c6e3d74764be417e4089faa113d054342e
Aurghyadip Kundu <adkundu@gmail.com>
2018-03-30 11:40:56 +0530

    Login UI created

---
+++ app/src/main/java/att2/com/wrc/aas/LoginActivity.java
@@ -0,0 +1,13 @@
+package att2.com.wrc.aas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
